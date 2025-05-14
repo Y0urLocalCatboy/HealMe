1358763bf66229da751620051540df8605bc4c21
Gabriel <gregoryhousemd@wp.pl>
2025-05-14 17:56:51 +0200

    Okej, wszystko co jestem w stanie zrobić to zrobione, dodać tylko prescriptions obsługę i powinno być doskonale

--- app/src/main/java/com/example/healme/viewmodel/DoctorViewModel.kt
+++ app/src/main/java/com/example/healme/viewmodel/DoctorViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.healme.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.healme.data.models.Prescription
 import com.example.healme.data.models.user.Doctor
 import com.example.healme.data.models.user.Patient
+import com.example.healme.data.models.user.User
 import com.example.healme.data.network.FirestoreClass
 
 class DoctorViewModel : ViewModel() {
@@ -14,4 +19,13 @@ class DoctorViewModel : ViewModel() {
     suspend fun getDoctorsPatients(id: String): List<Patient>? {
         return fs.patientsFromDoctor(id)
     }
+
+    suspend fun savePrescription(prescription: Prescription) {
+        TODO()
+    }
+
+    suspend fun loadPrescriptions() {
+        TODO()
+    }
+
 }
