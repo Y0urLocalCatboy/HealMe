5d4402018413dbaeb5f5a32e968503fc1f866c1f
Gabriel <gregoryhousemd@wp.pl>
2025-06-04 23:47:25 +0200

    Changed chat a bit

--- app/src/main/java/com/example/healme/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/example/healme/viewmodel/ChatViewModel.kt
@@ -5,8 +5,10 @@ import com.example.healme.R
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
+import kotlin.text.format
 
 /**
  * ViewModel for handling chat-related logic.
@@ -41,10 +43,25 @@ class ChatViewModel : ViewModel() {
      */
     fun formatTimestamp(timestamp: String): String {
         return try {
-            val date = Date(timestamp.toLong())
-            SimpleDateFormat("HH:mm dd/MM/yyyy", Locale.getDefault()).format(date)
+            val millis = timestamp.toLong()
+            val date = Date(millis)
+            val timeFormat = SimpleDateFormat("HH:mm", Locale("pl"))
+            val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale("pl"))
+
+            val now = Calendar.getInstance()
+            val messageTime = Calendar.getInstance().apply { timeInMillis = millis }
+
+            when {
+                now.get(Calendar.DATE) == messageTime.get(Calendar.DATE) ->
+                    "Today ${timeFormat.format(date)}"
+
+                now.get(Calendar.DATE) - messageTime.get(Calendar.DATE) == 1 ->
+                    "Yesteday ${timeFormat.format(date)}"
+                    //DODAĆ DODAĆ DODAĆ STRING RESOURCES DODAC DODAĆ TODO TODO TODO TODO
+                else -> "${dateFormat.format(date)} ${timeFormat.format(date)}"
+            }
         } catch (e: Exception) {
-            timestamp
+            "Date Error"
         }
     }
 }
