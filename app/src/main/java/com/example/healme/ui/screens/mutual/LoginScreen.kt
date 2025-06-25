804a361c039e53d6cd791c6e9c6760dba23e0c14
Michael Thomson <qwertytomson125@gmail.com>
2025-06-25 20:11:46 +0200

    added icon in LoginScreen

--- app/src/main/java/com/example/healme/ui/screens/mutual/LoginScreen.kt
+++ app/src/main/java/com/example/healme/ui/screens/mutual/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.healme.ui.screens.mutual
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -7,6 +8,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -36,6 +38,15 @@ fun LoginScreen(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logohealme),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .size(260.dp)
+                .padding(bottom = 16.dp)
+        )
+
+
         Text(
             text = stringResource(R.string.login_title),
             style = MaterialTheme.typography.headlineLarge,
@@ -127,6 +138,7 @@ fun LoginScreen(navController: NavController) {
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
